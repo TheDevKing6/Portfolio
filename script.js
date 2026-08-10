7a5992742fa042e62703fe0e2ed1afbea66cefc8
TheDevKing6 <92669188+TheDevKing6@users.noreply.github.com>
2026-08-10 15:55:12 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Fade-in on scroll
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) entry.target.classList.add('visible');
-  });
-}, { threshold: 0.1 });
-
-document.querySelectorAll('.section').forEach(sec => observer.observe(sec));
